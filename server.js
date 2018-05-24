c81b5b7b3dd96378d6d6c2f61d3244bcc402ea4e
Justyna Kaczorowska <js.kaczorowska@gmail.com>
2018-05-24 10:47:14 +0200

    Add auth/google

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.set('view engine', 'pug');
 app.set('views','./views');
 
 
-app.get('/first-template', function(req, res){
+app.get('/auth/google', function(req, res){
     res.render('first-template');
 });
 
@@ -16,7 +16,10 @@ app.get('/second-template', function (req, res) {
 
 
 
-app.listen(3000);
+app.listen(3000, 'localhost', function() {
+    console.log('Nasłuchuje na porcie 3000');
+});
+
 app.use(function (req, res, next) {
     res.status(404).send('Wybacz, nie mogliśmy odnaleźć tego, czego żądasz!')
 });
